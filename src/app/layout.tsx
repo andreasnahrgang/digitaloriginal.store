b26eed1b96cd0db85f5ada269db4b42b02becf2b
Andreas Nahrgang <andreas.nahrgang@gmail.com>
2025-11-21 08:27:56 +0100

    fix: remove thirdweb provider and lockfile to force clean build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
-        <ThirdwebProviderWrapper>{children}</ThirdwebProviderWrapper>
+        {children}
       </body>
     </html>
   );
